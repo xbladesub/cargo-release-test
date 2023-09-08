64b8ae18b66100415c3bc25dd39ec67ece445727
nshv <nickshv13@icloud.com>
2023-09-09 01:19:29 +0800

    c

---
+++ scripts/tag_release.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -eu
+
+# Checkout the master branch and ensure it's up to date
+git checkout main
+git pull --ff-only
+
+# Set the release version
+RELEASE=${RELEASE:-`date --iso-8601`}
+
+# Create and switch to the release branch
+git checkout -b release/${RELEASE}
+
+# Specify the version component (major, minor, or patch)
+COMPONENT=${COMPONENT:-patch}
+
+# Bump the version using cargo-release
+cargo release ${COMPONENT}
+
+# Push the changes and the newly created tag to the remote repository
+git push origin "${RELEASE_BRANCH}"
+git push --tags
