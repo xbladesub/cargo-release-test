8f63f40420cb188b71a868b40aabd3f86d0c569a
nshv <nickshv13@icloud.com>
2023-09-11 10:26:28 +0800

    09/11/2023 10:26:28 AM 💻

--- scripts/tag_release_old.sh
+++ scripts/tag_release_old.sh
@@ -1,12 +1,12 @@
 
 #!/bin/sh -eu
-# make sure local master branch is up to date
-git checkout master && git pull --ff-only
+# make sure local main branch is up to date
+git checkout main && git pull --ff-only
 RELEASE=${RELEASE:-`date --iso-8601`}
-# release branch is started from master
+# release branch is started from main
 git checkout -b release/${RELEASE}
 # major/minor/patch
 COMPONENT=${COMPONENT:-patch}
 # bump release version
-TAG=1 COMPONENT=$COMPONENT ./scripts/bumpversion.sh
+TAG=1 COMPONENT=$COMPONENT ./scripts/bumpversion_old.sh
 # git push && git push --tags
